908d579cd18455cac95a8a157a3068dbef4ffa03
edmond wen <edmond@cohere.com>
2026-01-30 15:43:10 +0000

    Adds  class for OpenAI inference

--- src/ensemble_phase_2_poc/inference/openai.py
+++ src/ensemble_phase_2_poc/inference/openai.py
@@ -1,8 +1,8 @@
 import backoff
-from langchain_cohere import ChatCohere
+from langchain_openai import ChatOpenAI
 from typing import Any
 
-class CustomChatCohere(ChatCohere):
+class CustomChatOpenAI(ChatOpenAI):
     @backoff.on_exception(backoff.expo, Exception, max_tries=5, jitter=backoff.full_jitter)
     def invoke(self, *args: Any, **kwargs: Any):
         return super().invoke(*args, **kwargs)
